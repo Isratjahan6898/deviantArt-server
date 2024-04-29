895c517a607a682dcf33287f198f4e2daad3c14d
israt6898 <israt6898@gmail.com>
2024-04-29 21:25:26 +0600

    ignore file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -45,6 +45,14 @@ async function run() {
       console.log(req.params.email);
       const result=await craftCollection.find({email:req.params.email}).toArray();
       res.send(result);
+    })
+
+    app.get('/craft-by-id/:id', async(req,res)=>{
+      const result = await craftCollection.findOne({ _id:new ObjectId(req.params.id),});
+      console.log(result);
+      res.send(result);
+      
+
     })
 
     app.post('/craft', async(req,res)=>{
